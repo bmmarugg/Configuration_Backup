89ad8fb8a9062eaa032a63a0389fb1f998979275
bmmarugg <45640963+bmmarugg@users.noreply.github.com>
2019-09-16 08:36:14 -0500

    Fixed Try and Except statements
    Fixed the Try and Except statements that allows for local credential use.

--- config_backup.py
+++ config_backup.py
@@ -41,23 +41,23 @@ def main():
     except:
         print("Could not connect to {} using domain creds. Trying local creds now... ".format(device))
 
-    try:
-        connect = netmiko.ConnectHandler(username=local_user, password=local_pass, device_type=ios, ip=device,
-                                         secret=enablepw)
-        if ">" in connect.find_prompt():
-            connect.enable()
-        print("Now connected to: {}".format(device))
-        device_config = open("path/to/config/repo/{}_{}-running_config.txt".format(today, device), 'w')
-
-        connect.send_command("term len 0")
-        run_config = connect.send_command("sh run")
-        device_config.write(run_config)
-        config_log.write("Running-config for: {} pulled successfully.".format(device))
-
-    except:
-        print("Could not connect to {}. Moving on...".format(device))
-        config_log.write("Could NOT pull running-config for: {}. Check device connectivity or enable password"
-                         .format(device))
+        try:
+            connect = netmiko.ConnectHandler(username=local_user, password=local_pass, device_type=ios, ip=device,
+                                             secret=enablepw)
+            if ">" in connect.find_prompt():
+                connect.enable()
+            print("Now connected to: {}".format(device))
+            device_config = open("path/to/config/repo/{}_{}-running_config.txt".format(today, device), 'w')
+    
+            connect.send_command("term len 0")
+            run_config = connect.send_command("sh run")
+            device_config.write(run_config)
+            config_log.write("Running-config for: {} pulled successfully.".format(device))
+    
+        except:
+            print("Could not connect to {}. Moving on...".format(device))
+            config_log.write("Could NOT pull running-config for: {}. Check device connectivity or enable password"
+                             .format(device))
 
 
 def fw_main():
@@ -67,7 +67,7 @@ def fw_main():
         if ">" in connect.find_prompt():
             connect.enable()
         print("Now connected to: {}".format(device))
-        device_config = open("path/to/config/repo/{}_{}-running_config.txt".format(today, device), 'w')
+        device_config = open("path/to/config/repo/{}_{}-running_config.txt" .format(today, device), 'w')
 
         connect.send_command("term pager 0")
         run_config = connect.send_command("sh run")
